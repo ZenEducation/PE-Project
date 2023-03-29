952c0b32071d5d245dbd2747afabfff3b2c29dbd
zigmint <rupeshr053@gmail.com>
2023-03-29 22:45:33 +0530

    testimonials

---
+++ components/Public/Testimonials.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="w-full dark:bg-gray-700">
+        <div
+            class="flex flex-col lg:grid lg:gap-4 2xl:gap-6 lg:grid-cols-4 2xl:row-span-2 2xl:pb-8 ml-2 pt-4 px-6 max-w-[1720px]">
+            <!-- Beginning of the component about Daniel Clifford -->
+            <div
+                class=" bg-white text-black dark:bg-gray-900 dark:text-white lg:order-1 lg:row-span-1 2xl:row-span-1 lg:col-span-2 rounded-lg shadow-xl dark:shadow-none mb-5 lg:mb-0">
+                <div class="mx-6 my-8 2xl:mx-10">
+                    <img class="w-8 md:w-9 lg:w-10 2xl:w-20 h-8 md:h-9 lg:h-10 2xl:h-20 rounded-full border-2 ml-1 lg:ml-3 2xl:ml-0 md:-mt-1 2xl:-mt-4"
+                        src="https://images.pexels.com/photos/3775534/pexels-photo-3775534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    <h1
+                        class="text-xs md:text-base    pl-12 lg:pl-16 2xl:pl-20 -mt-8 md:-mt-10 lg:-mt-11 2xl:-mt-20 2xl:mx-8">
+                        Aadya Deshpande
+                    </h1>
+                    <h2 class="text-opacity-50 text-xs md:text-base    pl-12 lg:pl-16 2xl:pl-20 2xl:my-2 2xl:mx-8">
+                        NEET All India Rank | 355
+                    </h2>
+                </div>
+                <div class="-mt-6 relative">
+                    <p class="text-xl   font-bold px-7 lg:px-9 2xl:pt-6 2xl:mx-2">
+                        My NEET performance would not have been possible without them!
+                    </p>
+                    <br />
+                    <!-- 390 length -->
+                    <p class=" text-opacity-50 font-medium md:text-sm  px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">
+                        My NEET performance would not have been possible without them! Their Early Completion of Syllabus
+                        Strategy enabled me to revise the entire syllabus more than 5 to 6 times, which was not possible for
+                        other students that I know from other institutes, as their syllabus was completed just 3 to 4 months
+                        before the NEET exam, in which case revision would not at all be possible for any student.See
+                        More... </p>
+                </div>
+            </div>
+            <!-- Ending of the component about Daniel Clifford -->
+
+            <!-- Beginning of the component about Jonathan Walters -->
+            <div
+                class="bg-white text-black dark:bg-gray-900 dark:text-white lg:order-2 lg:row-span-1 2xl:row-span-1 lg:col-span-1 rounded-lg shadow-xl dark:shadow-none pb-4 mb-5 lg:mb-0">
+                <div class="mx-8 2xl:mx-10 my-10">
+                    <img class="w-8 md:w-9 2xl:w-20 h-8 md:h-9 2xl:h-20 rounded-full border-2 -ml-1 -mt-2 lg:-mt-4"
+                        src="https://images.pexels.com/photos/634021/pexels-photo-634021.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    <h1 class=" text-xs md:text-base    pl-11 md:pl-12 2xl:pl-24 -mt-8 md:-mt-10 2xl:-mt-16">
+                        Saurabh Parekh
+                    </h1>
+                    <h2 class="text-xs md:text-base    text-opacity-50 pl-11 md:pl-12 2xl:pl-24">
+                        B-TECH | IIT BOMBAY</h2>
+                </div>
+                <div class="-mt-8 mx-1 lg:mx-2">
+                    <p class=" text-lg lg:text-xl   font-semibold pt-1 px-6 2xl:px-8 lg:pl-5 lg:pr-8">
+                        My experience was very wonderful.
+                    </p>
+                    <br />
+                    <!-- 347 length -->
+                    <p class=" text-opacity-50 font-medium md:text-sm  pl-6 lg:pl-5 pr-4 -mt-1  2xl:px-8">
+                        “ My experience was very wonderful. I have learnt a lot here. The professors at are very helpful.
+                        Due to the personalized teaching mode, I was able to clear all my doubts effectively. Getting my
+                        doubts clarified on time improved my practice test scores. I would strongly recommend JEE Advanced
+                        and JEE Main aspirants to enroll with them.See More... ”</p>
+                </div>
+            </div>
+            <!-- Ending of the component about Jonathan Walters -->
+
+            <!-- Beginning of the component about Jeanette Harmon -->
+            <div
+                class="bg-white text-black dark:bg-gray-900 dark:text-white lg:order-3 lg:row-span-2 2xl:row-span-1 lg:col-span-1 rounded-lg shadow-xl dark:shadow-none mb-5 lg:mb-0 ">
+                <div class="mx-8 my-10 lg:my-8">
+                    <img class="w-8 md:w-9 lg:w-11 2xl:w-20 h-8 md:h-9 lg:h-11 2xl:h-20 rounded-full border-2 -mt-3 -ml-1 lg:-ml-0"
+                        src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    <h1
+                        class=" text-xs md:text-base    pl-11 md:pl-12 lg:pl-14 2xl:pl-24 -mt-8 md:-mt-10 lg:-mt-11 2xl:-mt-16">
+                        Owais Chunawala </h1>
+                    <h2 class=" text-xs md:text-base    pl-11 md:pl-12 lg:pl-14 2xl:pl-24">
+                        B-TECH | IIT BOMBAY</h2>
+                </div>
+                <div class="-mt-4 ml-5 mr-11">
+                    <p class=" text-xl   font-bold px-2 lg:px-3 -mt-6 lg:-mt-5 2xl:mt-12 2xl:pb-6">
+                        I was guided every step of the way by the professors and mentors.</p>
+                    <br />
+                    <!-- 399 length -->
+                    <p class=" font-medium md:text-sm  pl-2 lg:pl-3 lg:pr-4 mb-6 2xl:pt-2 -mt-3">
+                        “ I was guided every step of the way by the professors and mentors. All my doubts were cleared very
+                        effectively and on-time. I was given numerous tips, tricks and techniques for efficient preparation,
+                        fast problem-solving and was made to solve 100s of questions from the learning sources, in a
+                        structured and systematic manner, and that greatly impacted my test scores and helped me gain
+                        confidence.See More... ”</p>
+                </div>
+            </div>
+            <!-- Ending of the component about Jeanette Harmon -->
+
+            <!-- Beginning of the component about Patrick Abrams -->
+            <div
+                class="bg-white text-black dark:bg-gray-900 dark:text-white lg:order-4 lg:row-span-2 2xl:row-span-1 col-span-2 rounded-lg shadow-xl dark:shadow-none mb-5 lg:mb-0 lg:pb-14">
+                <div class="mx-8 my-8">
+                    <img class="w-8 md:w-9 lg:w-10 2xl:w-20 h-8 md:h-9 lg:h-10 2xl:h-20 rounded-full border-2 lg:-mt-3"
+                        src="https://images.pexels.com/photos/3778603/pexels-photo-3778603.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    <h1 class="text-xs md:text-base    pl-12 md:pl-14 2xl:pl-24 -mt-8 md:-mt-10 lg:-mt-11 2xl:-mt-16">
+                        Ojas Deshpande </h1>
+                    <h2 class="text-xs md:text-base    text-opacity-50 pl-12 md:pl-14 2xl:pl-24">
+                        M.B.B.S | KING EDWARD MEMORIAL HOSPITAL AND SETH GORDHANDAS SUNDERDAS MEDICAL COLLEGE, MUMBAI</h2>
+                </div>
+                <div class="px-3 -mt-3 mb-5 lg:mb-0">
+                    <p class="text-lg   font-semibold px-4 -mt-3 lg:-mt-6 2xl:mt-8">
+                        The one to one teaching and mentoring methodology helped me in a great way.
+                    </p>
+                    <br />
+                    <p class="text-opacity-50 font-medium md:text-sm  px-4 mt-1 lg:-mt-3 2xl:mt-6">
+                        “
+                        <span class="text-black  dark:text-white">I would like to extend my thanks to Paathshala for my NEET
+                            score. Paathshala’s one to one
+                            teaching
+                            and mentoring methodology helped me in a great way. It allowed the professors to monitor my
+                            progress
+                            at each step. I received feedback pertaining to my areas of improvement and not just some
+                            general
+                            feedback that is applicable to everyone. That helped me immensely to focus on my weak areas
+                        </span>
+                        <span class="hidden sm:block text-black  dark:text-white">
+                            and strengthen them with time Paathshala’s strategy of early completion of syllabus has provided
+                            one of the biggest advantages by
+                            giving me 2 years of time only for practice, revision and test series enabling me to constantly
+                            improve my scores. The Full Syllabus Tests helped me understand the concepts that I had
+                            forgotten
+                            with time and also the topics and chapters in which I need to practice more. The professors
+                            clarified all my NEET doubts from time to time, making my revision and practice process
+                            effective
+                            and efficient. Paathshala’s Mistakes Analysis’ Report has been an amazing tool in helping me
+                            find
+                            out topics and chapters, where I needed more practice and revision. I have given 200 to 250 NEET
+                            Full Syllabus Tests. Solving these many FSTs greatly improved my time management skills and also
+                            my
+                            ability to stay calm and confident in an exam environment
+
+                            I would recommend Paathshala’s Educational services to all future aspirants. The personal
+                            attention
+                            Paathshala delivers is the key to success in competitive entrance exams </span>”
+                    </p>
+                </div>
+            </div>
+            <!-- Ending of the component about Patrick Abrams -->
+
+            <!-- Beginning of the component about Kira Whittle -->
+            <div
+                class="bg-white text-black dark:bg-gray-900 dark:text-white lg:order-2 lg:row-span-3 xl:row-span-3  2xl:row-span-2 mb-4 xl:mb-0 lg:col-span-1 rounded-lg shadow-xl dark:shadow-none">
+                <div class="mx-8 my-8 lg:pl-1">
+                    <img class="w-8 md:w-9 lg:w-12 2xl:w-20 h-8 md:h-9 lg:h-12 2xl:h-20 rounded-full border-2 lg:-mt-4 -ml-1 lg:-ml-4"
+                        src="https://images.pexels.com/photos/3762804/pexels-photo-3762804.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    <h1 class=" text-xs md:text-base    pl-10 md:pl-12 2xl:pl-24 -mt-8 md:-mt-10 lg:-mt-12 2xl:-mt-16">
+                        Nihar Mhapankar</h1>
+                    <h2 class=" text-xs md:text-base    pl-10 md:pl-12 2xl:pl-24">
+                        M.B.B.S | LOKMANYA TILAK GENERAL MEDICAL COLLEGE</h2>
+                </div>
+                <div class="px-3 lg:px-5 lg:-mt-4 mb-5 lg:mb-0">
+                    <p class=" text-xl   font-semibold px-4 lg:px-0 -mt-2 lg:-mt-0">
+                        To merely explain my experience and emotions with Paathshala with a few words? </p>
+                    <br />
+                    <p class=" font-medium md:text-sm  px-4 lg:px-0 2xl:px-4 lg:pr-3 mt-2 lg:-mt-1 2xl:mt-2 2xl:pb-64">
+                        “ To merely explain my experience and emotions with Paathshala with a few words? Oh Boy! It’s going
+                        to be a task between the Devil and the Deep Blue Sea. My 2-year experience here was an adventure.
+                        Professors here helped me a lot in teaching, doubts, and concept clarification. I would say that
+                        they are true experts in their subjects. The professors here are the best! The environment and
+                        ambiance of the classrooms were far beyond my expectation. Good infrastructure, comfortable benches,
+                        quality teaching facilities, you name it! Learning here was fun and the environment maintained by
+                        the teachers was the cherry on the cake. Our senior students also gave us the information about the
+                        mighty class in our 11 Standard. I was also given the right guidance about the books to be referred
+                        to, in addition to the study materials provided. I would rest my case on a final note that, if
+                        Paathshala is given the opportunity to compete at the higher level, it would gain all the honors and
+                        respect it truly deserves!! ”</p>
+                </div>
+            </div>
+            <!-- Ending of the component about Kira Whittle -->
+        </div>
+    </div>
+</template>
