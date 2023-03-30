d8124d37e643844d5b76413e3976932480b62bd2
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-03-30 12:54:55 +0530

    About Us Page Created

---
+++ components/Public/About/Paathshala_about.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2 class="text-center text-3xl text-[#3f3a64] dark:text-white font-bold">About Paathshala</h2>
+        <p class=" px-36 py-5 text-gray-300 m-5 text-justify dark:text-[#ddd]">We, at Paathshala, strive to redefine traditional education systems, which have, most often favored students who are perceived to be sharper and smarter. We strongly believe that every student deserves a fair opportunity to receive the best quality education and the right guidance from the right people, as that will enable the student to achieve his/her potential</p>
+        <h4 class="text-center text-xl text-black dark:text-white font-bold">Mission and Vision</h4>
+        <p class=" px-36 pb-10 text-gray-300 mx-5  text-justify dark:text-[#ddd]">“We aim to transform with utmost efficiency, each student, into the intellectually best version of themselves, with the underlying purpose of giving them the best possible chance of excelling in the career of their choice”</p>
+        <p class=" px-36  text-gray-300 mx-5  text-center dark:text-[#ddd]">Let’s keep Redefining Education With Paathshala!</p>
+        <p class=" px-36 text-gray-300 mx-5 text-center dark:text-[#ddd]">We are all in this together!</p>
+    </div>
+</template>
+
