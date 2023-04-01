4dba6aedeb0df69eb0fb07f89fef32dd165dd54b
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-04-01 18:16:13 +0530

    Updated in Enquiry with us

--- pages/About/enquire_with_us.vue
+++ pages/About/enquire_with_us.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="dark:bg-black bg-white">
-        <div class=" mx-auto bg-white dark:bg-black flex justify-center items-start">
-            <div class=" w-2/4  flex justify-center items-center mt-10">
-                <EnquiryLeftSection/>
-            </div>
-            <div class=" w-2/4  flex justify-center items-center mt-10">
-                <EnquiryRightSection/>
+        <div class=" mx-auto bg-white dark:bg-black flex flex-col justify-center  items-center">
+            <courseheader>
+                <template v-slot:title>Enquire-With-Us</template>
+            </courseheader>
+            <div class="flex justify-center  items-start mx-auto w-full">
+                <div class=" w-2/4  flex justify-center items-center mt-10">
+                    <EnquiryLeftSection />
+                </div>
+                <div class=" w-2/4  flex justify-center items-center mt-10">
+                    <EnquiryRightSection />
+                </div>
             </div>
         </div>
         <div class="bg-[#f8f8f8] dark:bg-[#161821] p-3 my-3">
