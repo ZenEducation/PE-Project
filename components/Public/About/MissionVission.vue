03552181b0e5804f4a7b6e9a2ec221b7429150a5
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-04-06 00:10:41 +0530

    About us section changes

--- components/Public/About/MissionVission.vue
+++ components/Public/About/MissionVission.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class=" grid grid-cols-2 m-10 px-10">
-        <div class=" flex justify-center ">
-            <img src="/images/about/about-us/mission-vission.png" class="w-2/4 rounded-2xl" alt="">
-        </div>
-        <div class="flex items-center flex-col justify-center ">
-            <h3 class="text-center text-3xl text-[#20ad96] mt-3">Mission and Vission</h3>
-            <p class="text-gray-300 m-5 text-justify dark:text-[#ddd]">“We aim to transform with utmost efficiency, each student, into the intellectually best version of themselves, with the underlying purpose of giving them the best possible chance of excelling in the career of their choice”</p>
-            <p class="text-gray-300 m-5 text-justify dark:text-[#ddd]">“In the process, we also provide career guidance, life coaching and motivation coaching”</p>
+    <div class=" grid grid-cols-1 m-10 px-10">
+        
+        <div class="flex items-center flex-col justify-center px-56 ">
+            <h3 class="text-center text-3xl text-[#20ad96] mt-3 ">Mission and Vission</h3>
+            <p class="text-gray-300 m-5 text-center dark:text-[#ddd] ">“We aim to transform with utmost efficiency, each student, into the intellectually best version of themselves, with the underlying purpose of giving them the best possible chance of excelling in the career of their choice”</p>
+            <p class="text-gray-300 m-5 text-center dark:text-[#ddd]">“In the process, we also provide career guidance, life coaching and motivation coaching”</p>
         </div>
 
     </div>
--- components/Public/About/Paathshala_about.vue
+++ components/Public/About/Paathshala_about.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <h2 class="text-center text-3xl text-[#3f3a64] dark:text-white font-bold">About Paathshala</h2>
-        <p class=" px-36 py-5 text-gray-300 m-5 text-justify dark:text-[#ddd]">We, at Paathshala, strive to redefine traditional education systems, which have, most often favored students who are perceived to be sharper and smarter. We strongly believe that every student deserves a fair opportunity to receive the best quality education and the right guidance from the right people, as that will enable the student to achieve his/her potential</p>
-        <h4 class="text-center text-xl text-black dark:text-white font-bold">Mission and Vision</h4>
-        <p class=" px-36 pb-10 text-gray-300 mx-5  text-justify dark:text-[#ddd]">“We aim to transform with utmost efficiency, each student, into the intellectually best version of themselves, with the underlying purpose of giving them the best possible chance of excelling in the career of their choice”</p>
-        <p class=" px-36  text-gray-300 mx-5  text-center dark:text-[#ddd]">Let’s keep Redefining Education With Paathshala!</p>
-        <p class=" px-36 text-gray-300 mx-5 text-center dark:text-[#ddd]">We are all in this together!</p>
+        <h2 class="text-left text-3xl text-[#3f3a64] dark:text-white font-bold px-36">About Paathshala</h2>
+        <p class=" px-36 py-5 text-gray-300 my-5 text-left dark:text-[#ddd]">We, at Paathshala, strive to redefine traditional education systems, which have, most often favored students who are perceived to be sharper and smarter. We strongly believe that every student deserves a fair opportunity to receive the best quality education and the right guidance from the right people, as that will enable the student to achieve his/her potential</p>
+        <h4 class="text-left text-xl text-black dark:text-white font-bold px-36">Mission and Vision</h4>
+        <p class=" px-36 pb-10 text-gray-300 my-5  text-left dark:text-[#ddd]">“We aim to transform with utmost efficiency, each student, into the intellectually best version of themselves, with the underlying purpose of giving them the best possible chance of excelling in the career of their choice”</p>
+        <p class=" px-36  text-gray-300 my-5  text-left dark:text-[#ddd]">Let’s keep Redefining Education With Paathshala!</p>
+        <p class=" px-36 text-gray-300 mys-5 text-left dark:text-[#ddd]">We are all in this together!</p>
     </div>
 </template>
 
