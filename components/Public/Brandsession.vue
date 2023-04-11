21fc77d247c9c1ad18eb5aa3759047137ba9b8f9
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-04-11 15:55:38 +0530

    Testimonial section, Blog Details Section, BrandSession

--- components/Public/Brandsession.vue
+++ components/Public/Brandsession.vue
@@ -1,10 +1,62 @@
 <template>
-  <div class="py-[100px] bg-[#161821]">
-    <div class="px-20">
-      <div class=""></div>
+  <div class="py-[50px] px-[50px] bg-white dark:bg-[#161821] flex items-center justify-center">
+    <div class=" h-[100px] m-auto overflow-hidden relative w-auto">
+      <div class="animate-[scroll_40s_linear_infinite] flex w-[calc(250px_*_14)] ">
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="images/brand/client-logo-01.png" alt="can't show">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-02.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-03.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-04.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-05.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-06.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-07.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="images/brand/client-logo-01.png" alt="can't show">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-02.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-03.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-04.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-05.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-06.png" alt="">
+        </div>
+        <div class="h-[100px] w-[250px] flex justify-center items-center">
+          <img src="/images/brand/client-logo-07.png" alt="">
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
+@keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-250px * 7));
+  }
+  
+}
 </style>
