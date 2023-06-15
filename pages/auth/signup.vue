a66b3f1228bb8699b771e776b834eca12e0b2397
ashir <garbage.meet@gmail.com>
2023-06-15 16:10:28 +0530

    Updated Component Location over signup page

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,90 +1,90 @@
 <script setup>
-  import { reactive } from 'vue'
-  import { useRouter } from 'vue-router'
-  import { mdiAccount, mdiAsterisk } from '@mdi/js'
-  import { Auth } from 'aws-amplify'
-  import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth'
-  import SectionFullScreen from '@/components/AfterAuth/Sections/SectionFullScreen.vue'
-  import CardBox from '@/components/AfterAuth/Cards/CardBox.vue'
-  import FormField from '@/components/AfterAuth/Forms/FormField.vue'
-  import FormControl from '@/components/AfterAuth/Forms/FormControl.vue'
-  import BaseButton from '@/components/AfterAuth/Buttons/BaseButton.vue'
-  import BaseButtons from '@/components/AfterAuth/Buttons/BaseButtons.vue'
-  import {
-    useAuthStore,
-    signUp,
-    resendConfirmationCode,
-    confirmSignUp,
-  } from '@/stores/auth'
-  import BaseDivider from '@/components/AfterAuth/Navbar/BaseDivider.vue'
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import { mdiAccount, mdiAsterisk } from '@mdi/js'
+import { Auth } from 'aws-amplify'
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth'
+import SectionFullScreen from '@/components/AfterAuth/Sections/SectionFullScreen.vue'
+import CardBox from '@/components/AfterAuth/Cards/CardBox.vue'
+import FormField from '@/components/AfterAuth/Forms/FormField.vue'
+import FormControl from '@/components/AfterAuth/Forms/FormControl.vue'
+import BaseButton from '@/components/AfterAuth/Buttons/BaseButton.vue'
+import BaseButtons from '@/components/AfterAuth/Buttons/BaseButtons.vue'
+import {
+  useAuthStore,
+  signUp,
+  resendConfirmationCode,
+  confirmSignUp,
+} from '@/stores/auth'
+import BaseDivider from '@/components/AfterAuth/NavBar/BaseDivider.vue'
 
-  const form = reactive({
-    email: '',
-    username: '',
-    familyName: '',
-    remember: true,
-    verificationCode: '',
-  })
+const form = reactive({
+  email: '',
+  username: '',
+  familyName: '',
+  remember: true,
+  verificationCode: '',
+})
 
-  const authStore = useAuthStore()
-  const router = useRouter()
-  const validPasswordRegex =
-    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
-  const verificationCodeMsg = ref('')
-  const errMsg = ref('')
-  const resendCodeMsg = ref('')
-  function showErr(msg) {
-    errMsg.value = msg
+const authStore = useAuthStore()
+const router = useRouter()
+const validPasswordRegex =
+  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
+const verificationCodeMsg = ref('')
+const errMsg = ref('')
+const resendCodeMsg = ref('')
+function showErr(msg) {
+  errMsg.value = msg
+}
+function emptyErr() {
+  errMsg.value = ''
+}
+
+const createAccount = async () => {
+  emptyErr()
+  console.log('inside create account')
+  resetState()
+  authStore.emptyUser()
+  const userData = await signUp({ ...form })
+  if (userData.isUserCreated) {
+    verificationCodeMsg.value = `Verification code sent to ${userData.codeDeliveryDetails.AttributeName} ${userData.codeDeliveryDetails.Destination}`
+    authStore.setupUser(userData)
+  } else {
+    showErr(userData.msg)
   }
-  function emptyErr() {
-    errMsg.value = ''
-  }
+}
 
-  const createAccount = async () => {
-    emptyErr()
-    console.log('inside create account')
-    resetState()
-    authStore.emptyUser()
-    const userData = await signUp({ ...form })
-    if (userData.isUserCreated) {
-      verificationCodeMsg.value = `Verification code sent to ${userData.codeDeliveryDetails.AttributeName} ${userData.codeDeliveryDetails.Destination}`
-      authStore.setupUser(userData)
-    } else {
-      showErr(userData.msg)
-    }
-  }
-
-  const verifyAccount = async () => {
-    emptyErr()
-    if (form.verificationCode) {
-      const userData = await confirmSignUp({
-        username: form.email,
-        code: form.verificationCode,
-      })
-      if (userData) {
-        if (!userData.isUserVerified) {
-          showErr(userData.msg)
-        } else {
-          navigateTo({ path: '/dashboard' })
-        }
+const verifyAccount = async () => {
+  emptyErr()
+  if (form.verificationCode) {
+    const userData = await confirmSignUp({
+      username: form.email,
+      code: form.verificationCode,
+    })
+    if (userData) {
+      if (!userData.isUserVerified) {
+        showErr(userData.msg)
+      } else {
+        navigateTo({ path: '/dashboard' })
       }
     }
   }
+}
 
-  const resendCode = async () => {
-    emptyErr()
-    const resp = await resendConfirmationCode({ username: form.email })
-    if (resp.isCodeSent) {
-      resendCodeMsg.value = `Verification code sent on ${resp.AttributeName} to ${resp.Destination}`
-    }
-    setTimeout(() => {
-      resendCodeMsg.value = ''
-    }, 3000)
+const resendCode = async () => {
+  emptyErr()
+  const resp = await resendConfirmationCode({ username: form.email })
+  if (resp.isCodeSent) {
+    resendCodeMsg.value = `Verification code sent on ${resp.AttributeName} to ${resp.Destination}`
   }
+  setTimeout(() => {
+    resendCodeMsg.value = ''
+  }, 3000)
+}
 
-  function resetState() {
-    verificationCodeMsg.value = ''
-  }
+function resetState() {
+  verificationCodeMsg.value = ''
+}
 </script>
 
 <template>
@@ -100,30 +100,15 @@
           </CardsCardBoxComponentBody>
 
           <FormField label="Email" help="Please enter your email">
-            <FormControl
-              v-model.trim="form.email"
-              :icon="mdiAccount"
-              name="login"
-              autocomplete="email"
-            />
+            <FormControl v-model.trim="form.email" :icon="mdiAccount" name="login" autocomplete="email" />
           </FormField>
 
           <FormField label="Username" help="Please enter your username">
-            <FormControl
-              v-model.trim="form.username"
-              :icon="mdiAccount"
-              name="username"
-              autocomplete="username"
-            />
+            <FormControl v-model.trim="form.username" :icon="mdiAccount" name="username" autocomplete="username" />
           </FormField>
 
           <FormField label="Family name" help="Please enter your family name">
-            <FormControl
-              v-model.trim="form.familyName"
-              :icon="mdiAccount"
-              name="familyName"
-              autocomplete="familyName"
-            />
+            <FormControl v-model.trim="form.familyName" :icon="mdiAccount" name="familyName" autocomplete="familyName" />
           </FormField>
 
           <NotificationBar v-if="errMsg" color="danger">
@@ -131,11 +116,7 @@
           </NotificationBar>
 
           <BaseButtons class="flex items-center justify-center">
-            <BaseButton
-              color="info"
-              label="Create account"
-              @click="createAccount"
-            />
+            <BaseButton color="info" label="Create account" @click="createAccount" />
           </BaseButtons>
 
           <div class="flex items-center mt-6">
@@ -153,11 +134,7 @@
           </CardsCardBoxComponentBody>
 
           <FormField label="Please enter your verification code">
-            <FormControl
-              v-model.trim="form.verificationCode"
-              :icon="mdiAccount"
-              name="code"
-            />
+            <FormControl v-model.trim="form.verificationCode" :icon="mdiAccount" name="code" />
           </FormField>
 
           <NotificationBar v-if="resendCodeMsg" color="success">
@@ -169,47 +146,26 @@
 
           <BaseButtons class="flex items-center justify-center">
             <BaseButton color="info" label="Resend code" @click="resendCode" />
-            <BaseButton
-              color="info"
-              label="Verify account"
-              @click="verifyAccount"
-            />
+            <BaseButton color="info" label="Verify account" @click="verifyAccount" />
           </BaseButtons>
         </div>
 
         <template #footer>
-          <BaseButtons
-            v-if="!verificationCodeMsg"
-            class="flex items-center justify-center mt-0"
-          >
-            <BaseButton
-              color="info"
-              label="Google"
-              @click="
-                () =>
-                  Auth.federatedSignIn({
-                    provider: CognitoHostedUIIdentityProvider.Google,
-                  })
-              "
-            >
+          <BaseButtons v-if="!verificationCodeMsg" class="flex items-center justify-center mt-0">
+            <BaseButton color="info" label="Google" @click="() =>
+                Auth.federatedSignIn({
+                  provider: CognitoHostedUIIdentityProvider.Google,
+                })
+              ">
             </BaseButton>
-            <BaseButton
-              color="info"
-              label="Facebook"
-              @click="
-                () =>
-                  Auth.federatedSignIn({
-                    provider: CognitoHostedUIIdentityProvider.Facebook,
-                  })
-              "
-            />
+            <BaseButton color="info" label="Facebook" @click="() =>
+                Auth.federatedSignIn({
+                  provider: CognitoHostedUIIdentityProvider.Facebook,
+                })
+              " />
           </BaseButtons>
           <BaseButtons v-else class="flex items-center justify-center mt-0">
-            <BaseButton
-              color="info"
-              label="Sign up with different email"
-              @click="resetState"
-            />
+            <BaseButton color="info" label="Sign up with different email" @click="resetState" />
           </BaseButtons>
         </template>
       </CardBox>
